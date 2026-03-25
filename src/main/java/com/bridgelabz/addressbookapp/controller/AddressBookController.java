59d603c03f9a73e92953abc5b001f03fe214cc42
preethi-r04 <preethiliki2004@gmail.com>
2026-03-25 12:05:20 +0530

    [Preethi]: Added: Section2 UC1: AddressBook DTO and Model

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,10 +1,17 @@
 package com.bridgelabz.addressbookapp.controller;
 
+import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+import com.bridgelabz.addressbookapp.model.AddressBook;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressBookController {
+
+    @PostMapping("/create")
+    public AddressBook createAddress(@RequestBody AddressBookDTO dto) {
+        return new AddressBook(dto);
+    }
 
     // GET
     @GetMapping("")
